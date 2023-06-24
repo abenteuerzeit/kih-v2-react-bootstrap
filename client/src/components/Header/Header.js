90a95e354b5877c98163872a32a90f624e0d9554
abenteuerzeit <dr.adrian.mroz@outlook.com>
2023-06-24 22:32:59 +0200

    feat: Add react-icons to project and implement dark mode toggle in Header component
    Added the `react-icons` package to project dependencies to use icons in UI. Also, implemented a dark mode toggle feature in Header component using the `BsMoon` and `BsSun` icons from `react-icons`.

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,110 +1,51 @@
-// // Header.js
-// import React, { useState } from "react";
-// import "./Header.css";
-// import logo from "../../static/logo-long.svg";
-
-// const Header = () => {
-//   return (
-//     <nav class="navbar navbar-expand-lg bg-body-tertiary">
-//       <div class="container-fluid">
-//         <a
-//           class="navbar-brand"
-//           href="http://www.kulturaihistoria.umcs.lublin.pl/en/"
-//         >
-//           <img src={logo} className="logo" alt="Kultura i Historia logo" />
-//         </a>
-//         <button
-//           class="navbar-toggler"
-//           type="button"
-//           data-bs-toggle="collapse"
-//           data-bs-target="#navbarSupportedContent"
-//           aria-controls="navbarSupportedContent"
-//           aria-expanded="false"
-//           aria-label="Toggle navigation"
-//         >
-//           <span class="navbar-toggler-icon"></span>
-//         </button>
-//         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-//           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-//             <li class="nav-item">
-//               <a
-//                 class="nav-link active"
-//                 aria-current="page"
-//                 href="http://www.kulturaihistoria.umcs.lublin.pl/en/"
-//               >
-//                 Home
-//               </a>
-//             </li>
-//             <li class="nav-item">
-//               <a
-//                 class="nav-link"
-//                 href="http://www.kulturaihistoria.umcs.lublin.pl/pl/kultura-i-histora-nr-42-2022-2"
-//               >
-//                 Latest Issue
-//               </a>
-//             </li>
-//             <li class="nav-item">
-//               <a
-//                 class="nav-link"
-//                 href="http://www.kulturaihistoria.umcs.lublin.pl/pl/numery"
-//               >
-//                 Archives
-//               </a>
-//             </li>
-//             <li class="nav-item">
-//               <a
-//                 class="nav-link"
-//                 href="http://www.kulturaihistoria.umcs.lublin.pl/pl/dla-autorow"
-//               >
-//                 For Authors
-//               </a>
-//             </li>
-//           </ul>
-//           <form class="d-flex" role="search">
-//             <input
-//               class="form-control me-2"
-//               type="search"
-//               placeholder="Search"
-//               aria-label="Search"
-//             />
-//             <button class="btn btn-outline-success" type="submit">
-//               Search
-//             </button>
-//           </form>
-//         </div>
-//       </div>
-//     </nav>
-//   );
-// };
-
-// export default Header;
-
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import { useState, useEffect } from 'react';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+// import NavDropdown from "react-bootstrap/NavDropdown";
+import logo from "./img/logo.svg";
+import { BsMoon, BsSun } from "react-icons/bs";
 
 function Header() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.setAttribute('data-bs-theme', 'dark');
+    } else {
+      document.documentElement.setAttribute('data-bs-theme', 'light');
+    }
+  }, [darkMode]);
+
+  const handleDarkModeToggle = () => {
+    setDarkMode(!darkMode);
+  }
+
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" className={darkMode ? "bg-dark" : "bg-light"}>
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="http://www.kulturaihistoria.umcs.lublin.pl/en/">
+          <img 
+            src={logo}               
+            alt="Kultura i Historia logo" 
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+            />{' '}
+            Kultura i Historia
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link href="http://www.kulturaihistoria.umcs.lublin.pl/en/">Home</Nav.Link>
+            <Nav.Link href="http://www.kulturaihistoria.umcs.lublin.pl/pl/kultura-i-histora-nr-42-2022-2">Latest Issue</Nav.Link>
+            <Nav.Link href="http://www.kulturaihistoria.umcs.lublin.pl/pl/numery">Archives</Nav.Link>
+            <Nav.Link href="http://www.kulturaihistoria.umcs.lublin.pl/pl/dla-autorow">For Authors</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link onClick={handleDarkModeToggle}>
+              {darkMode ? <BsSun /> : <BsMoon />}
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
