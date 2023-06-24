b399cdbc12cdedba9bbf53f68e96700d899f60c5
abenteuerzeit <dr.adrian.mroz@outlook.com>
2023-06-24 22:43:15 +0200

    Refactor Header component with updated Bootstrap and toggle function
    This commit updates the Header component by importing the latest React Bootstrap dependencies and changing the attribute names from single quotes to double quotes. Additionally, the Navbar's responsive menu is now controlled with the "responsive-navbar-nav" id and the container class is now fluid. Finally, the handleDarkModeToggle function is slightly revised to toggle the state of darkMode.

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
@@ -11,36 +11,43 @@ function Header() {
 
   useEffect(() => {
     if (darkMode) {
-      document.documentElement.setAttribute('data-bs-theme', 'dark');
+      document.documentElement.setAttribute("data-bs-theme", "dark");
     } else {
-      document.documentElement.setAttribute('data-bs-theme', 'light');
+      document.documentElement.setAttribute("data-bs-theme", "light");
     }
   }, [darkMode]);
 
   const handleDarkModeToggle = () => {
     setDarkMode(!darkMode);
-  }
+  };
 
   return (
     <Navbar expand="lg" className={darkMode ? "bg-dark" : "bg-light"}>
-      <Container>
+      <Container fluid>
         <Navbar.Brand href="http://www.kulturaihistoria.umcs.lublin.pl/en/">
-          <img 
-            src={logo}               
-            alt="Kultura i Historia logo" 
+          <img
+            src={logo}
+            alt="Kultura i Historia logo"
             width="30"
             height="30"
             className="d-inline-block align-top"
-            />{' '}
-            Kultura i Historia
+          />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="http://www.kulturaihistoria.umcs.lublin.pl/en/">Home</Nav.Link>
-            <Nav.Link href="http://www.kulturaihistoria.umcs.lublin.pl/pl/kultura-i-histora-nr-42-2022-2">Latest Issue</Nav.Link>
-            <Nav.Link href="http://www.kulturaihistoria.umcs.lublin.pl/pl/numery">Archives</Nav.Link>
-            <Nav.Link href="http://www.kulturaihistoria.umcs.lublin.pl/pl/dla-autorow">For Authors</Nav.Link>
+            <Nav.Link href="http://www.kulturaihistoria.umcs.lublin.pl/en/">
+              Home
+            </Nav.Link>
+            <Nav.Link href="http://www.kulturaihistoria.umcs.lublin.pl/pl/kultura-i-histora-nr-42-2022-2">
+              Latest Issue
+            </Nav.Link>
+            <Nav.Link href="http://www.kulturaihistoria.umcs.lublin.pl/pl/numery">
+              Archives
+            </Nav.Link>
+            <Nav.Link href="http://www.kulturaihistoria.umcs.lublin.pl/pl/dla-autorow">
+              For Authors
+            </Nav.Link>
           </Nav>
           <Nav>
             <Nav.Link onClick={handleDarkModeToggle}>
